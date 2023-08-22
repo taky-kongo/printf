e3dd0369e81be1f8601d98939423c834be897796
Kongo Malick <malicktakykongo@gmail.com>
2023-08-22 23:32:28 +0000

    Update the files

--- prints.c
+++ prints.c
@@ -27,3 +27,29 @@ int print_char(va_list args)
 		return (-1);
 	return (1);
 }
+
+/**
+ * print_string - a function that prints a string
+ * @args: the arguments
+ *
+ * Return: a number
+ */
+int print_string(va_list args)
+{
+	char *str;
+	int count = 0;
+	int value;
+
+	str = va_arg(args, char *);
+	if (str == NULL)
+		str = "(nil)";
+	while (*str)
+	{
+		value = _putchar(*str);
+		if (value == -1)
+			return (-1);
+		count++;
+		str++;
+	}
+	return (count);
+}
