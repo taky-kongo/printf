821f7bf261f34f84a6396a8f63095426a180c27f
Kongo Malick <malicktakykongo@gmail.com>
2023-08-22 22:23:59 +0000

    Update the file main.h

--- main.h
+++ main.h
@@ -4,8 +4,22 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <unistd.h>
+
+/**
+ * struct str - Struct str
+ *
+ * @ch: the string
+ * @f: the function associated
+ */
+typedef struct str
+{
+	char *ch;
+	(*f)(valist);
+} list_str;
 
 int _printf(const char *format, ...);
+int _putchar(int c);
 int (*get_func(const char *format))(va_list);
 
 #endif
