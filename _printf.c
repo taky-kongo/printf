2150932b93e07e1d3b4c33fe04a210f9631deb97
taky-kongo <abdoulmalickkongo@gmail.com>
2023-08-22 23:38:30 +0000

    Add the file _printf.c

---
+++ _printf.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * _printf - a function that produces output according to a format.
+ * @format: the arguments to enter
+ *
+ * Return: a number
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int i = 0;
+	int (*f)(va_list);
+	int count = 0;
+	const char *c;
+
+	va_start(args, format);
+	while (format[i])
+	{
+		if (format[i] == '%' && format[i + 1] != '%')
+		{
+			i++;
+			c = &format[i];
+			f = get_func(c);
+			if (f == NULL)
+			{
+				count += _putchar(format[i]);
+			}
+			else if (format[i] == '\0')
+			{
+				return (-1);
+			}
+			else
+			{
+				count += f(args);
+			}
+		}
+		i++;
+	}
+	va_end(args);
+	return (count);
+}
